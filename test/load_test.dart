e11139fa470abe9acceba07827ae295f33467fab
Caijinglong <cjl_spy@163.com>
2019-05-21 16:00:38 +0800

    remove test

--- test/load_test.dart
+++ test/load_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:load/load.dart';
+// import 'package:load/load.dart';
 
 void main() {
   test('adds one to input values', () {});
