33600be3e2ab8137340d0b86ad36bd7ab65b7fd0
shakirfattani <m.shakirfattani@gmail.com>
2021-06-14 20:39:36 +0500

    just none nullable return type changes

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jinglongcai/sdk/flutter/fvm/versions/2.0.0"
-export "FLUTTER_APPLICATION_PATH=/Users/jinglongcai/code/flutter/self/flutter_load_widget/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=K:\android\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\apps\flutter_load_widget\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
